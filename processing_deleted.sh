8b5d519ff2b02849bac9a1fcdd39c85818703ade
nicissimo97 <nici.regista@gmail.com>
2017-05-07 21:28:35 +0200

    Update processing_deleted.sh

--- processing_deleted.sh
+++ processing_deleted.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
 file_path="$1"
+suffix=".RESYNCH.mkv"
 
-//Function
+# Function #
 
 removePath () {
   echo "$file_path" | sed 's/...............................................//'
 }
+
 addResynch () {
-	"${1}.RESYNCH.mkv"
+	"${1}$suffix"
 }
 
-//Main
+# Main #
 
 file_name=$(removePath "$file_path")
 file_name_resynch=$(addResynch "$file_name")
@@ -21,6 +23,6 @@ if [[ $file_name == *mkv ]] || [[ $file_name == *mp4 ]]; then
 	rm "file_name_resynch"
 else
 	cd $file_path
-  rm -rf "$file_path"
+  	rm -rf "$file_path"
   
 fi
