6dc0335a67b2499ebdedb240812c411ccde5f430
nicissimo97 <nici.regista@gmail.com>
2018-04-03 22:53:44 +0200

    py commit

---
+++ VideoProcessingPy/autoVideoProcessing.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# Auto Video Processing for rTorrent
+# Version: 1.1
+#
+# Copyright (C) 2018 Nicolas D'amuri - nicissimo97
+#
+import sys
+
+SUBFIX = "RESYNCH.mkv"
+fullPath = "/media/sdah1/nicissimo97/private/rtorrent/RESYNCH/"
+pathManualMode = fullPath + "data";
+filePath = sys.argv[1]
