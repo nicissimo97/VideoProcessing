302ac74dc117409a54dfcece00db37e4d390367d
nicissimo97 <nici.regista@gmail.com>
2018-04-03 23:12:38 +0200

    code polishing

--- VideoProcessing Bash/processing.sh
+++ VideoProcessing Bash/processing.sh
@@ -6,60 +6,61 @@
 #
 # Changelog
 
-file_path="$1" 
-path_manual_mode="/media/sdah1/nicissimo97/private/rtorrent/data"
-full_path="/media/sdah1/nicissimo97/private/rtorrent/RESYNCH/"  
+subfix = "RESYNCH.mkv"
+filePath="$1"
+pathManualMode="/media/sdah1/nicissimo97/private/rtorrent/data"
+fullPath="/media/sdah1/nicissimo97/private/rtorrent/RESYNCH/"
 removePath () {
-  echo "$file_path" | sed 's/...............................................//'
+  echo "$filePath" | sed 's/...............................................//'
 }
 
-file_name=$(removePath "$file_path")
+fileName=$(removePath "$filePath")
 
 # Logging #
 for (( i = 0; i < 100; i++ )); do
-  echo -ne "*">>$full_path/log.txt
+  echo -ne "*">>$fullPath/log.txt
 done
-echo "">>$full_path/log.txt
-echo "$file_name">>$full_path/log.txt
+echo "">>$fullPath/log.txt
+echo "$fileName">>$fullPath/log.txt
 
 # Functions #
 encode () {
-	eval ffmpeg -y -i "$2" -s 96x54 -crf 30 -c:a mp3 -b:a 64k -ac 2 "${2}.RESYNCH.mkv">>$full_path/log.txt
+	eval ffmpeg -y -i "$2" -s 96x54 -crf 30 -c:a mp3 -b:a 64k -ac 2 "${2}$subfix">>$fullPath/log.txt
 }
 
 # Main #
-if [[ $file_path ]]; then
-if [[ $file_name == *mkv ]] || [[ $file_name == *mp4 ]] || [[ $file_name == *avi ]]; then
-	encode "$file_path" "$file_path"
-	echo "[ 1 OK! ]">>$full_path/log.txt
+if [[ $filePath ]]; then
+if [[ $fileName == *mkv ]] || [[ $fileName == *mp4 ]] || [[ $fileName == *avi ]]; then
+	encode "$filePath" "$filePath"
+	echo "[ 1 OK! ]">>$fullPath/log.txt
 else
-	cd $file_path
+	cd $filePath
 	for i in *.rar;
 	do
 		rar x $i;
-		video_name=$(ls *.mkv | head -1)
-		encode "$file_path/$video_name" "$file_path/$video_name"
-		echo "[ 2 OK! ]">>$full_path/log.txt
+		videoName=$(ls *.mkv | head -1)
+		encode "$filePath/$videoName" "$filePath/$videoName"
+		echo "[ 2 OK! ]">>$fullPath/log.txt
 	done;
 
 	for y in *.mkv;
 	do
-		encode "$file_path/$y" "$file_path/$y"
-		video_name=$(ls *.mkv | head -1)
-		echo "$file_path/$y">>$full_path/log.txt
-		echo "[ 3 OK! ]">>$full_path/log.txt
+		encode "$filePath/$y" "$filePath/$y"
+		videoName=$(ls *.mkv | head -1)
+		echo "$filePath/$y">>$fullPath/log.txt
+		echo "[ 3 OK! ]">>$fullPath/log.txt
 	done;
 fi
 else
 	echo "[ ERROR ] Empty variable - Manual Mode"
-	echo "[ ERROR ]">>$full_path/log.txt
-	cd "$path_manual_mode"
+	echo "[ ERROR ]">>$fullPath/log.txt
+	cd "$pathManualMode"
 	eval "ls"
 	echo "Select video file and press ENTER:  "
 	read file
-	encode "$path_manual_mode/$file" "$path_manual_mode/$file"
+	encode "$pathManualMode/$file" "$pathManualMode/$file"
 fi
 # Clear variable content #
-full_path=""
-file_path=""
-file_name=""
+fullPath=""
+filePath=""
+fileName=""
